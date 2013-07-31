380cc80a4f50143f6f7f8409d00eec0b650c42b0
Grace Wong <gwongz@gmail.com>
2013-07-31 13:33:16 -0700

    Problem with passing pixel to first_column_with_pixel_color fcn

--- segments.py
+++ segments.py
@@ -2,112 +2,83 @@ import PIL
 from SimpleCV import Image, Color
 
 
-def first_column_with_pixel_color(color, start, width, height, table):
+def make_table(img, width, height):
+	"""Creates image grid with y columns and x rows"""
+
+	table = [[ 0 for y in range(width)] for x in range(height) ]
+	# populates table with pixel values of img
+	for y in range(width):
+		for x in range(height):
+			table[x][y] = pixelMap[y, x]
+
+	return table
+
+
+
+def first_column_with_pixel_color(color, start, width, height, pixels):
+	
+
 	for y in range(start, width):
 		for x in range(height):
 
-			pixel = table[y, x]
-			if pixel == color:
-				# x is column number 
-				return x 
+			cpixel = pixels[x,y]
+			if cpixel == color:
+				# y is column number 
+				return y 
 
 	return None
 
+# test prints 		
+# first = first_column_with_pixel_color(0, 0)
+# second = first_column_with_pixel_color(255, first)
+# third = first_column_with_pixel_color(0, second)
+# print first, second, third
 
-def scan_image(width, height, table):
+
+def scan_image(width, height, pixels):
 	"""Finds columns where image should be segmented"""
-	
-
 	looking_for = 0 # first look for black
 	starting_col = 0 # start with first column
 	boundaries = []
 
-	print "This is initialized looking_for", looking_for
-	print "This is initialized starting_col", starting_col
+	while starting_col != None:
 
-
-	while starting_col != None:	
-		print "This is starting_col at top", starting_col
-		print "This is looking_for at top", looking_for
-		
-		next_col = first_column_with_pixel_color(looking_for, starting_col, width, height, table)
-		print "This is next_col", next_col
-		
+		next_col = first_column_with_pixel_color(looking_for, starting_col, width, height, pixels)
 		if next_col != None:
-			boundaries.append(next_col)			
+			boundaries.append(next_col)
+		if next_col == 0: # if you found black in first iteration, start looking for white
 			looking_for = 255
-
 		else:
-			looking_for = 0
-		starting_col = next_col
-		
-		
-		print "This is starting_col at bottom ", starting_col
-		print "This is next_col at bottom ", next_col
-		print "This is looking_for at bottom ", looking_for	
-		
+			looking_for = 0	
+		starting_col = next_col 
 
-	slices = [(x, 0) for x in boundaries if x!= None] # a list of tuples 
-	
-	return slices 
+
+	return boundaries
+
 
 
 def main():
-	imgname = 'rockwell_a.png'
+	imgname = 'bw.png'
 	img = PIL.Image.open(imgname)
 	if img.mode != '1':
 		img = img.convert('1')
-	table = img.load()
 	width = img.size[0]
 	height = img.size[1]
-	# print width
-	# print height
+	pixels = img.load()
+	all_pixels = []
+	for x in range(width):	
+		for y in range(height):
+			cpixel = pixels[x,y]
+			all_pixels.append(cpixel)
 
+	bounds = scan_image(width, height, pixels)
+	print bounds 
 
-	slices = scan_image(width, height, table)
-
-  
-
-if __name__ == "__main__":
+if __name__== "__main__":
 	main()
 
- 
 
 
- # def get_letter(img, slices):
-# 	"""Crops image at slices coordinates and saves"""
-# 	# opens cropped image 
-# 	img = PIL.Image.open(img)
-# 	height = img.height
-# 	width = img.width
-
-# 	for item in slices:
-# 		n = slices.index(item)
-# 		width = slices[n+1][0] - slices[n][0]
-
-# 		if n+1 < len(slices)-1:
-# 			print "This is the item: ", item
-# 			print "This is the width: ", width
-# 			print "This is n", n 
-# 			letter = cropped_image.crop(item[0], item[1] width, height)
-# 			letter.save('letter.%s') % (n)
-
-# 		else:
-# 			break
-	
-	# this is an image cropped from original 
-	# do i need to return anything here if I'm just saving an image?
-
-# letter = get_letter('rockwell_a.png', slices)
-
-# def process_letter(img): # takes segmented letter as input
-# 	"""Crops segmented letter so it is optimized for matching"""
-# 	img = Image(img)
-# 	binarize = img.binarize() # SimpleCV img class
-# 	blobs = img.findBlobs() # uses SimpleCV blobs fcn
-# 	bounds = blobs[-1].boundingBox() # SimpleCV fcn
-# 	img = img.crop(bounds)
-# 	img.save('user_img.png') # returns a cropped image object 
 
 
 
