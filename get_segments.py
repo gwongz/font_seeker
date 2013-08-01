ae4e43f3d36caa84da716ab40f07fd381892b4d8
Grace Wong <gwongz@gmail.com>
2013-08-01 15:38:57 -0700

    Get image from command line

--- get_segments.py
+++ get_segments.py
@@ -1,3 +1,4 @@
+from sys import argv
 import PIL
 from SimpleCV import Image
 
@@ -6,7 +7,7 @@ from SimpleCV import Image
 def load_image(imgname):
 	img = PIL.Image.open(imgname)
 	if img.mode != '1':
-		img.convert('1')
+		img = img.convert('1')
 
 	width = img.size[0]
 	height = img.size[1]
@@ -42,10 +43,12 @@ def get_column(grid): # stores rows from image grid as columns
 def first_black(width, height, columns, current_col):
 	
 	while current_col < width:
-		if columns[current_col].count(0) > 1:
+		if columns[current_col].count(0) >= 1:
+		
 			return current_col	
 		else:
 			current_col +=1
+		
 	
 def all_white(width, height, columns, current_col):
 	while current_col < width:
@@ -61,10 +64,15 @@ def all_white(width, height, columns, current_col):
 
 def scan_image(width, height, columns):
 	boundaries = []
-	current_col=0
-	while current_col < width and current_col != None:
+	current_col = 0
 
+	while current_col < width and current_col != None:
+		
+		
 		next_col = first_black(width, height, columns, current_col)
+		
+		print "This is next_col after finding first black", next_col
+		
 		if next_col == None:
 			break # there is no black - prevents infinite loop 
 
@@ -76,26 +84,77 @@ def scan_image(width, height, columns):
 			if white_col != None: # if there is an all white column
 				boundaries.append(white_col)
 				current_col = white_col # reset starting point for scanning
+
+		print "Boundaries at bottom of while loop", boundaries
+		print "current_col at bottom of while loop", current_col
+
+	slices = [(x, 0) for x in boundaries if x!= None] # a list of tuples, (x,y) where splits are 
 	
-	return boundaries
+	return slices
+
+def get_letter(slices, height, imgname):
+	# slices = [(0,0), (1,0)]
+	# slices = [(35,0), (67,0)]
+
+
+
+
+	n=0
+	
+	while n < len(slices)-1:
+		print "This is n at the start: ", n
+		for item in slices:
+			print "This is n at the top of for loop: ", n
+			print "This is the item at the top of loop: ", item
+			# print "Item[n]: ", item[n]
+			# print "Item[n+1]: ", item[n+1]
+			width = (slices[n+1][0]) - (slices[n][0]) # width of each crop
+			# print "Width: ", width
+			print "Width at top of loop for n: ", width, n 
+
+			
+			# width = slices[1][0] - slices[0][0]
+			# width = 1-0 = 1
+
+			# if 1 <= 1: True 
+
+			# print "This is item[n]: ", item[n]
+			box = (item[n], item[n+1], width, height)
+			# print box
+			print "Box at the top of the loop:", box 
+			if n+1 <= len(slices)-1: 
+				
+			
+				img = PIL.Image.open(imgname) # how can I avoid reopening the img?
+				if img.mode != '1':
+					img.convert('1')
+				letter = img.crop(box)
+				letter.save('user_crop.png') # saves image to current directory as 'letter'
+			
+				n += 2
+
+				if n > len(slices)-1:
+					break
+
+			else:
+				break
+
 
 
 def main():
-	imgname = 'test.png'
+	script, input_file = argv
+	imgname = input_file
+
 	img_width_height_columns = load_image(imgname) # loads basic img information
 
 	width = img_width_height_columns[0]
 	height = img_width_height_columns[1]
 	columns = img_width_height_columns[2]
 
+	slices = scan_image(width, height, columns)
+	print "These are the bounds", slices 
 
-
-	
-	bounds = scan_image(width, height, columns)
-	print bounds 
-
-
-	
+	get_letter(slices, height, imgname)
 
 
 
