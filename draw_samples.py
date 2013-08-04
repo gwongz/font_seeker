f02b828d3fd096a562c14d12a56462fb3efc2cb4
Grace Wong <gwongz@gmail.com>
2013-08-04 15:38:28 -0700

    Adds crop blob function so font samples are cropped before added to database in seed.py

--- draw_samples.py
+++ draw_samples.py
@@ -1,18 +1,26 @@
 import os
 import string
 import PIL
-from PIL import Image, ImageDraw, ImageFont
+from SimpleCV import Image, ImageSet
+# from SimpleCV import Image
+
+# from PIL import Image, ImageDraw, ImageFont
+
 
 
 
 """Draws uppercase and lowercase samples for fonts located in 'fonts' directory"""
 
-def make_font_directories(root_directory):
+def make_templates(root_directory):
 
 	if not os.path.exists(root_directory):
 		os.mkdir(root_directory)
 	
 	ttfs = os.listdir(root_directory) 
+
+	if ttfs == []:
+		print "There are no font files in this directory."
+		return
 
 	if '.DS_Store' in ttfs:
 			ttfs.remove('.DS_Store')
@@ -31,50 +39,51 @@ def make_font_directories(root_directory):
 			# makes sub-directory for uppercase samples
 			os.mkdir(font_directory+'/upper') # makes a sub folder for uppercase fonts
 
+		lower = string.ascii_lowercase
+		upper = string.ascii_uppercase
 		
 		# draws samples for each font 
-		draw_lower(fontpath, shortname, font_directory)
-		draw_upper(fontpath, shortname, font_directory+'/upper')
+		draw_letters(fontpath, shortname, font_directory, lower)
+		draw_letters(fontpath, shortname, font_directory+'/upper', upper)
 
 
 
-def draw_lower(fontpath, shortname, font_directory): 
+def draw_letters(fontpath, shortname, font_directory, letter_range): 
 
-	for letter in string.ascii_lowercase:
+	for letter in letter_range:
 		letterpath = os.path.abspath(os.path.join(font_directory, letter+'.png'))
 		
 		# if sample file doesn't already exist, then make one
 		if not os.path.exists(letterpath): 
 			W, H = (600, 600)
-			img = Image.new('1', (W,H), 'white')	
-			font = ImageFont.truetype(fontpath, 350) 
-			draw = ImageDraw.Draw(img)
+			img = PIL.Image.new('1', (W,H), 'white')	
+			font = PIL.ImageFont.truetype(fontpath, 350) 
+			draw = PIL.ImageDraw.Draw(img)
 			# w, h = draw.textsize(letter)
 			# draw.text(((W-w)/4,(H-h)/10), letter, font=font, fill='black')
 			draw.text((W/5,H/5), letter, font=font, fill='black')			
 			# saves new img to font_directory as 'a.png', 'b.png', etc.	
 			img.save(letterpath, quality = 100)
 
-def draw_upper(fontpath, shortname, font_directory):
-	
-	for letter in string.ascii_uppercase:
-		letterpath = os.path.abspath(os.path.join(font_directory, letter+'.png'))
-	
-		if not os.path.exists(letterpath):
-			W, H = (600,600)
-			img = Image.new('1', (W,H), 'white')	
-			font = ImageFont.truetype(fontpath, 350) 
-			draw = ImageDraw.Draw(img)
-			# w, h = draw.textsize(letter)
-			draw.text((W/5, H/5), letter, font=font, fill='black')			
-			# saves new img to font_directory as 'A.png', 'B.png', etc.	
-			img.save(letterpath, quality = 100)
+def crop_letters(directory):
 
-
+	# looks for .png sample files in fonts directory and appends to dict by letter 
+	for dirpath, dirnames, fnames in os.walk(directory):
+	    for f in fnames:
+	        if f.endswith('.png'):
+	        	location = os.path.join(dirpath, f)
+	        	img = Image(location) # uses SimpleCV Image class 
+	        	blobs = img.binarize().findBlobs()
+	        	bounds = blobs[-1].boundingBox()
+	        	img.crop(bounds).save(location)
+	        
 def main():
 
-	root_directory = 'templates'
-	make_font_directories(root_directory)
+	# root_directory = 'templates'
+	# make_templates(root_directory)
+	training_alphabet = make_templates('training_alphabet')
+	crop_letters('training_alphabet')
+
 
 if __name__== "__main__":
 	main()
