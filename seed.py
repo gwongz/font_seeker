f337b5a9e4aaa53cb329d8ffeecf7a6410c81652
Grace Wong <gwongz@gmail.com>
2013-08-03 19:02:52 -0700

    Adds templates dictionary and crop function

--- seed.py
+++ seed.py
@@ -1,35 +1,76 @@
 """Loads fonts from local directory into database"""
-
-import model
-import os.path 
+ 
+import os 
 import re 
+from SimpleCV import Image
 
-from PIL import Image
 
-os.path.join("path1", "path2", font)
+def get_samples(directory):
 
-def load_A (session):
-	home = "/Users/gwongz/src/hackbright/project/font_files/A"
-	font = "A.AppleGothic.png"
-	path = os.path.join(home, font)
+	samples_dict = {}
+
+	# looks for .png sample files in fonts directory and appends to dict by letter 
+	for dirpath, dirnames, fnames in os.walk(directory):
+	    for f in fnames:
+	        if f.endswith('.png'):
+	        	location = os.path.join(dirpath, f)
+	        	letter = f.split('.')[0]
+	        	samples_dict.setdefault(location, letter)
+	  				
+	return samples_dict
+
+def crop_letters(samples_dict):
+	for key in samples_dict.iterkeys():
+		img = Image(key)
+		binarize = img.binarize()
+		blobs = binarize.findBlobs()
+		bounds = blobs[-1].boundingBox()
+		crop = img.crop(bounds)
+		crop.save(key)
+
+def load_letters(samples_dict):
+
+	for key in samples_dict.iterkeys():
+		file_url = key
+		value = samples_dict[key]
+		print key, value 
+
+
+
+
+
+
+
+
+	    
+def main():
+	directory = 'templates'
+	font_samples = get_samples(directory)
 	
-	with Image.open(path) as f:
-		font = model.Letter_A(id = ord('A'),
-							fontname = "AppleGothic",
-							path = path)
-		session.add(font)
-	session.commit()
+	crop_letters(font_samples)
 
-
-
-# filename = os.path.join('path', 'to', 'image', 'file')
-# img = Image.open(filename)
-# print img.size
-
-
-def main(session):
-	load_A(session)
 
 if __name__ == "__main__":
-	s = model.session
-	main(s)
+	main()
+	        	 
+
+
+
+
+
+
+
+
+
+# img = Image('fonts/Arial/a.png')
+# print img.height 
+
+# def main(session):
+# 	directory = 'fonts'
+# 	crop_samples(directory)
+
+	
+
+# if __name__ == "__main__":
+# 	s = model.session
+# 	main(s)
