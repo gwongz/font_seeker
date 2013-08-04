7bdad8d330c92e1ed16b1a90e3930de616b04d6d
Grace Wong <gwongz@gmail.com>
2013-08-03 17:00:13 -0700

    Traverses font folder and makes A-Z and a-z samples of each font

--- draw_samples.py
+++ draw_samples.py
@@ -1,88 +1,76 @@
-import string, re
 import os
+import string
+import PIL
 from PIL import Image, ImageDraw, ImageFont
 
 
-"""Draws A-Z and a-z for fonts located in "font_files" directory"""
 
-def get_fonts():
+"""Draws uppercase and lowercase samples for fonts located in 'fonts' directory"""
+
+def make_font_directories(root_directory):
 	
-		fontpath = 'font_files'
-		ttfs = os.listdir(fontpath)
+	ttfs = os.listdir(root_directory) 
 
-		if '.DS_Store' in ttfs:
+	if '.DS_Store' in ttfs:
 			ttfs.remove('.DS_Store')
 
-		for font in ttfs:
-			fontname = re.sub('\s', '-', font) #filename
-			os.rename(path+font, path+fontname)
-			shortname = re.search('.+(?=\.)', fontname).group() #name without file ext
+	for fontfile in ttfs:
+		# fontpath is where the ttf file is located 
+		fontpath = os.path.join(root_directory, fontfile) 
+	
+		# shortname is just name of the font 
+		(shortname, extension) = os.path.splitext(fontfile)
+		font_directory = os.path.join(root_directory, shortname)
 
-			#make font object
-			font = Font(name=shortname)
-			db_session.add(font)
-			db_session.flush()
-			db_session.refresh(font)
+		# makes new directory for font if it doesn't already exist
+		if not os.path.exists(font_directory): # if the font_directory doesn't exist, make it
+			os.mkdir(font_directory) # makes directory for each font 
+			# makes sub-directory for uppercase samples
+			os.mkdir(font_directory+'/upper') # makes a sub folder for uppercase fonts
 
-			load_fixed_font_imgs(font, path)
-
-
-def draw_alphabet(fontpaths, dirname, fonts):
-
-	for count in range(0,52):
-		if count < 26:
-			letter = chr(ord('A') + count)
-			letter_object = Letter(value=letter, upper=True, pointsize=pointsize, fixed_width=width, fixed_height=height)
-			db_session.add(letter_object)
-			db_session.commit()
-
-			os.mkdir(root_directory+'/fixed/'+letter+'-upper/')
-			os.mkdir(root_directory+'/constrained/'+letter+'-upper/')
 		
-			
+		# draws samples for each font 
+		draw_lower(fontpath, shortname, font_directory)
+		draw_upper(fontpath, shortname, font_directory+'/upper')
+
+
+
+def draw_lower(fontpath, shortname, font_directory): 
+
+	for letter in string.ascii_lowercase:
+		letterpath = os.path.abspath(os.path.join(font_directory, letter+'.png'))
+		
+		# if sample file doesn't already exist, then make one
+		if not os.path.exists(letterpath): 
+			W, H = (300, 300)
+			img = Image.new('1', (W,H), 'white')	
+			font = ImageFont.truetype(fontpath, 250) 
+			draw = ImageDraw.Draw(img)
+			w, h = draw.textsize(letter)
+			draw.text(((W-w)/4,(H-h)/4), letter, font=font, fill='black')			
+			# saves new img to font_directory as 'a.png', 'b.png', etc.	
+			img.save(letterpath, quality = 100)
+
+def draw_upper(fontpath, shortname, font_directory):
+	
 	for letter in string.ascii_uppercase:
-		img = Image.new('1', (200,250), 'white')
-		fontpath = os.path.join(dirname, font)
-		font = ImageFont.truetype(fontpath, 150)
-		draw = ImageDraw.Draw(img)
-		draw.text((0,0), letter, font=font, fill='black')
-		letterpath = os.path.join(dirname, letter)
-		print letterpath
-		# img.save(letterpath
-
-
+		letterpath = os.path.abspath(os.path.join(font_directory, letter+'.png'))
 	
-	# fontPath = "font_files"
-
-	# pattern = re.search(".[\w]+", name)
-	# name = pattern.group()
-	# font = ImageFont.truetype(fontPath, 150)
-	# for letter in string.ascii_uppercase:
-	# 	img = Image.new('1', (200, 250), 'white')
-	# 	draw = ImageDraw.Draw(img)
-	# 	draw.text((0,0), letter, font = font, fill= "black")
-	# 	fontpath = os.path.join(fonts_directory, )
-	
-
-		# img.save('/font_files/%s.%s.png' % (letter, name))
-		# will need to decide where in dir to save these images 
+		if not os.path.exists(letterpath):
+			W, H = (300, 300)
+			img = Image.new('1', (W,H), 'white')	
+			font = ImageFont.truetype(fontpath, 250) 
+			draw = ImageDraw.Draw(img)
+			w, h = draw.textsize(letter)
+			draw.text(((W-w)/4,(H-h)/4), letter, font=font, fill='black')			
+			# saves new img to font_directory as 'A.png', 'B.png', etc.	
+			img.save(letterpath, quality = 100)
 
 
 def main():
-# 	# script, input_file = argv
-# 	# fontname = input_file
-	dirname = "font_files"
-	fonts = []
-	
-			fonts.append(font)
-	
 
-	print fonts
-	print fontPath
+	root_directory = 'fonts'
+	make_font_directories(root_directory)
 
-
-
-
-# if __name__== "__main__":
-# 	main()
-draw_alphabet('font_files')
+if __name__== "__main__":
+	main()
