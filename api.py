e50686cc9ab1a225e704cbd2ba8ec41d9fe18c1f
Grace Wong <gwongz@gmail.com>
2013-08-08 17:57:03 -0700

    Initial commit

--- api.py
+++ api.py
@@ -17,16 +17,16 @@ def get_all_fonts():
 @app.route('/send_image', methods = ['GET'])
 def send_image():
 	imgname = request.args.get('img')
+	# has to be passed in without 
 	segments = get_segments.main(imgname)
 	result = json.dumps(segments)
-	return "Your image has been segmented"
+	return "Your image has been processed"
 	
 	# http%3A%2F%2Fwww.flickr.com%2Fphotos%2graciferwong%29466743943%2F 
 	# hello_world.jpg:http://www.flickr.com/photos/graciferwong/9466743943/
 	# fox_novocento.png: http://www.flickr.com/photos/graciferwong/9466743485/
 	#graceserver.com/send_image?img=http%40%30%30hackbright%16com%30hello%16jpg
 	
-
 @app.route ('/match_letter', methods = ['GET'])
 def letter():
 	my_font = match_letter.main()
