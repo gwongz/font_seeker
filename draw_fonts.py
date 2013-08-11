93bfee1eb220dbaeff6e3a7dcab04d9d68a41a83
Grace Wong <gwongz@gmail.com>
2013-08-11 15:30:58 -0700

    Adds condition in make_letter_samples that allows for single-named fonts

--- draw_fonts.py
+++ draw_fonts.py
@@ -1,34 +1,24 @@
 import os
 import string
 import shutil
-
 from PIL import Image, ImageOps, ImageFont, ImageDraw, ImageChops
-import SimpleCV as cv 
-# from SimpleCV import Image
 
 
+"""Draws upper and lower letter samples with ttf files in 'font_files' and 'ocr_alphabet' directories"""
 
+def make_letter_samples(srcdir, destdir): 
 
-
-def make_directories(srcdir, destdir): 
-
-	root = "."
 	ttfs = os.listdir(srcdir)
 
 	if '.DS_Store' in ttfs:
 		ttfs.remove('.DS_Store')
 
-	# os.chdir(directory)
-
 	for f in ttfs:
 		fontpath = os.path.join(srcdir, f)
 		full_name = f.split('.')[0:-1]
-		full_name = f.split('-')[0:-1]
+		if '-' in full_name:
+			full_name = f.split('-')[0:-1]
 		font_name = "".join(full_name)
-		# font_directory = os.path.join(directory, font_name)
-
-		# if os.path.exists(font_directory):
-		# 	shutil.rmtree(font_directory)
 
 		currentpath = os.getcwd()
 		newpath = os.path.join(currentpath, destdir)
@@ -46,27 +36,19 @@ def make_directories(srcdir, destdir):
 		os.mkdir(upper)
 		os.mkdir(lower)
 
-
 		print "Font directory: ", font_directory
 		print "Fontpath: ", fontpath 
 
-	
 		draw_lower(fontpath, font_directory)
-	# makes uppercase and lowercase directories for font samples 
+		draw_upper(fontpath, font_directory)
 
 def draw_lower(fontpath, font_directory):
 
-
-
-	# fontpath = 'fonts/Arial.ttf'
 	W, H = 100, 100
 	font = ImageFont.truetype(fontpath, 75)
-	# font_directory = os.path.join('fonts/Arial', 'lower')
 
 	lower_directory = os.path.join(font_directory, 'lower')
 	print "This is the lower_directory:", lower_directory
-
-	# os.chdir(lower_directory)
 	
 	for letter in string.ascii_lowercase:
 		letterfile = letter+'.png'
@@ -74,81 +56,39 @@ def draw_lower(fontpath, font_directory):
 		
 		img = Image.new('1', (W,H), 'white')	
 		draw = ImageDraw.Draw(img)
+		w, h = draw.textsize(letter, font=font) # centers the text 
+	
+		print "This is the w, h:", w, h  
+		draw.text(((W-w)/2, (H-h)/2), letter, font=font, fill='black')			
+
+		new_img = img.save(letterpath)
+
+def draw_upper(fontpath, font_directory):
+
+	W, H = 100, 100
+	font = ImageFont.truetype(fontpath, 75)
+
+	upper_directory = os.path.join(font_directory, 'upper')
+	
+	for letter in string.ascii_uppercase:
+		letterfile = letter+'.png'
+		letterpath = os.path.join(upper_directory, letterfile)
+		
+		img = Image.new('1', (W,H), 'white')	
+		draw = ImageDraw.Draw(img)
 		w, h = draw.textsize(letter, font=font)
 	
 		print "This is the w, h:", w, h # centers the text 
-			# w, h = draw.textsize(letter)
-			# draw.text(((W-w)/4,(H-h)/10), letter, font=font, fill='black')
-
-		# draw.text((W/2)-w, (H/2)-h, letter, font=font, fill='black')	
 		draw.text(((W-w)/2, (H-h)/2), letter, font=font, fill='black')			
-			# saves new img to font_directory as 'a.png', 'b.png', etc.	
 
 		new_img = img.save(letterpath)
 
 
-
-# Load the image
-def constrained (mypath):
-
-	
-	files = os.listdir(mypath)
-	if '.DS_Store' in files:
-		files.remove('.DS_Store')
-
-	for f in files:
-		location = os.path.abspath(os.path.join(mypath, f))
-		file_url = os.path.join(mypath, f)
-		name = str(file_url)
-
-		img = cv.Image(location).invert()
-		adaptimg = img.adaptiveScale((20, 20), cv.Color.WHITE)
-		adaptimg.save(name)
-
-
-def crop_at_bounds(mypath):
-	
-	segments = os.listdir(mypath)
-
-	if '.DS_Store' in segments:
-		segments.remove('.DS_Store')
-
-	for imgfile in segments:
-		location = os.path.abspath(os.path.join(mypath, imgfile))
-		file_url = os.path.join(mypath, imgfile)
-		name = str(file_url)
-		print location 
-		img = cv.Image(location)
-		
-		
-		blobs = img.binarize().findBlobs()
-	
-		if blobs != None:
-			bounds = blobs[-1].boundingBox()
-			crop = img.crop(bounds).save(name)
-
-		if blobs == None:
-			os.remove(file_url)
-
-
-
 def main():
 
-	make_directories('testing_font_directories', 'font_letters')
-	# root = 'ocr_alphabet/Arial'
-	# make_directories(root)
-	# ocr_samples = draw_lower()
-	# crop_at_bounds('ocr_alphabet/Arial/lower')
-	# constrained('ocr_alphabet/Arial/lower')
+	make_letter_samples(srcdir='ocr_font_files', destdir='ocr_alphabet')
+	# make_letter_samples(srdir='font_files', destdir='font_letters')
 
-	# root = 'user_image'
-	# crop_at_bounds('user_image')
-	# constrained('user_image')
-
-	
-	# letter_samples = draw_lower()
-	# crop_at_bounds('fonts/Arial/lower')
-	# constrained('fonts/Arial/lower')
 
 
 if __name__ == "__main__":
