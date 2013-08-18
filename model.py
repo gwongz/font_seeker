9e92bf39109ca06456761353a11d6ee280dccea3
Grace Wong <gwongz@gmail.com>
2013-08-18 16:42:50 -0700

    Includes radius, area as features of letter classes

--- model.py
+++ model.py
@@ -30,10 +30,7 @@ class OCR_Letter(Base):
 	black_pixels = Column(Integer)
 	width = Column(Integer)
 	height = Column(Integer)
-	aspect_ratio = Column(Integer)
-	blob_area = Column(Integer)
-
-	
+	aspect_ratio = Column(Float)
 
 class Letter(Base):
 	__tablename__ = 'letters'
@@ -45,15 +42,27 @@ class Letter(Base):
 	font_name = Column(String(100))
 	width = Column(Integer)
 	height = Column(Integer)
-	aspect_ratio = Column(Integer)
-	blob_area = Column(Integer)
+	aspect_ratio = Column(Float)
+
 
 	font_id = Column(Integer, ForeignKey('fonts.id'))
 	font = relationship('Font', backref=backref('fonts', order_by=id))
 
 
-	
-	# add an is serif column?	
+class Letter_Feature(Base):
+	__tablename__ = 'letter_features'
+
+	id = Column(Integer, primary_key = True, autoincrement = True)
+	file_url = Column(String(150))
+	value = Column(Integer)
+	blob_radius = Column(Float)
+	blob_area = Column(Float)
+	aspect_ratio = Column(Float)
+
+	font_name = Column(Integer, ForeignKey('fonts.name'))
+	font = relationship('Font', backref=backref('font', order_by=id))
+
+
 class User_Image(Base):
 	__tablename__ = 'user_images'
 
@@ -61,16 +70,28 @@ class User_Image(Base):
 	file_url = Column(String(150))
 	width = Column(Integer)
 	height = Column(Integer)
+	aspect_ratio = Column(Float)
 
 	black_pixels = Column(Integer)
-	aspect_ratio = Column(Integer)
-	blob_area = Column(Integer)
 
 	ocr_letter_black_pixels = Column(Integer, ForeignKey('ocr_letters.black_pixels'))
 	letter_black_pixels = Column(Integer, ForeignKey('letters.black_pixels'))
 
 	ocr_letter = relationship('OCR_Letter', backref=backref('ocr_letters', order_by=id))
 	letter = relationship('Letter', backref = backref('letters', order_by=id))
+
+
+
+
+
+class User_Feature(Base):
+	__tablename__ = 'user_features'
+
+	id = Column(Integer, primary_key = True, autoincrement = True)
+	file_url = Column(String(150))
+	blob_radius = Column(Float)
+	blob_area = Column(Float)
+	aspect_ratio = Column(Float)
 
 
 def main():
