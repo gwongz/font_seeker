5414637f15828bada99745cfe6079101e45310d0
Grace Wong <gwongz@gmail.com>
2013-08-19 15:47:34 -0700

    Updates throughout

--- match_fonts.py
+++ match_fonts.py
@@ -1,8 +1,8 @@
 import os
 import re
 import numpy as np 
-import cProfile 
-from sys import argv
+from collections import OrderedDict
+
 from PIL import Image, ImageOps
 import SimpleCV as cv
 
@@ -12,11 +12,10 @@ from process_images import crop_at_bounds, make_constrained
 
 
 
-"""Identifies letter of alphabet for each segment stored in 'user' directory and then matches to font"""
+"""Triple nested loop - breaks when a font has been returned as a match 5 or more times"""
 
 def process_user_image(directory):
 
-	
 	crop_at_bounds(directory)
 	make_constrained(directory)
 
@@ -39,7 +38,7 @@ def get_letter(user_dir, ocr_dir):
 	segments = os.listdir(user_dir)
 	if '.DS_Store' in segments:
 		segments.remove('.DS_Store')
-		segments = sorted_nicely(segments)
+	sorted_segments = sorted_nicely(segments)
 
 	ocr_alphabet = []
 	for dirpath, dirnames, fnames in os.walk(ocr_dir):
@@ -47,12 +46,12 @@ def get_letter(user_dir, ocr_dir):
 	       		if f.endswith('.png'):
 	       			ocr_alphabet.append(f)
 
-	return segments, ocr_alphabet # lists in alphanumeric order 
+	return sorted_segments, ocr_alphabet # lists in alphanumeric order 
 
 
 
 def find_match(user_dir, ocr_dir, segments, ocr_alphabet):
-	"""Runs faster but no good real methodology for accuracy"""
+	"""Runs faster but arbitrary break points"""
 
 	font_table = {}
 	
@@ -62,22 +61,27 @@ def find_match(user_dir, ocr_dir, segments, ocr_alphabet):
 	for imgfile in segments:
 
 		img_url = os.path.abspath(os.path.join(user_dir, imgfile))	
-		user_img = Image.open(img_url).convert('1') # using PIL 
+		user_img = img_url # using SimpleCV
+
+		relative = user_img.split('/')[-2:]
+		relative_url = os.path.join(relative[0], relative[1])
+		print "This is relative_url", relative_url # this is for user black pixels look up 
 	
 		count = 0 
 		for letter in ocr_alphabet:
 			print "Count at the top of the OCR alphabet loop", count 
 
+			# if count equal to e or i or I, skip 
 			if count >= 26:
 				letter_url = os.path.abspath(os.path.join(ocr_dir+'/upper', letter))
 
 			else:
 				letter_url = os.path.abspath(os.path.join(ocr_dir+'/lower', letter))
 			
-			letter = Image.open(letter_url).convert('1')
-			diff = difference_of_images(user_img, letter)
+			letter = letter_url
+			ocr_xor = difference_of_images(user_img, letter)
 
-			print "This is the difference %s between %s and %s" % (diff, img_url, letter_url)
+			print "This is the difference %s between %s and %s" % (ocr_xor, img_url, letter_url)
 	
 			# initializes value for db lookup 		
 			if count < 26:
@@ -86,190 +90,140 @@ def find_match(user_dir, ocr_dir, segments, ocr_alphabet):
 			if count  >= 26:
 				value = count + 39
 
-
-
-			if diff <= 0.005:
+			if ocr_xor <= 0.2:
 				print "The difference was low enough to check the font"
-				print "The letter value %d is being looked up for %s" % (value, img_url)
+				print "The letter value %d is being looked up for %s" % (ocr_xor, img_url)
 
 				font_objects = model.session.query(model.Letter.file_url).filter(model.Letter.value == value).all()
 
+				print "Now let's narrow down the font objects by number of black pixels"
+
+				user_black_pixels_tuple = model.session.query(model.User_Image.black_pixels).filter(model.User_Image.file_url==relative_url)
+		
+				# print "This is userblack pixels tuple", user_black_pixels_tuple 
+				user_black_pixels = user_black_pixels_tuple[0][0]
+				print "This is the number of black pixels in user image:", user_black_pixels
+
+
 				for i in range (len(font_objects)):
 					font_location = str(font_objects[i][0])
-					font_img = Image.open(font_location).convert('1')
-					font_diff = difference_of_images(user_img, font_img)
+				
+					font_black_pixels_tuple = model.session.query(model.Letter.black_pixels).filter(model.Letter.file_url == font_location)
+					font_black_pixels = font_black_pixels_tuple[0][0]
 
-					if font_diff < 0.03:
+					black_pixels_min = user_black_pixels - 22.5 # range of 5% 
+					black_pixels_max = user_black_pixels + 22.5 
 
-						print "The difference was low enough to append to the dictionary"
-						print "This font location %s is a match for segment %s" % (font_location, img_url)
-						
-						font_name = model.session.query(model.Letter.font_name).filter(model.Letter.file_url==font_location).first()
+					if font_black_pixels > black_pixels_min and font_black_pixels < black_pixels_max:
 
-						if font_name not in font_table.keys():
-							font_table.setdefault(font_name, 0)
-						else:
-							font_table[font_name] += 1 
+						font_img = font_location
+						font_xor = difference_of_images(user_img, font_img)
+
+						if font_xor < 0.10:
+
+							print "The difference was low enough to append to the dictionary"
+							print "This font location %s is a match for segment %s" % (font_location, img_url)
+							
+							font_name = model.session.query(model.Letter.font_name).filter(model.Letter.file_url==font_location).first()
+
+							if font_name not in font_table.keys():
+								font_table.setdefault(font_name, 0)
+							else:
+								font_table[font_name] += 1 
+
+				most_matches = max(font_table.items(), key=lambda t: t[1])
+				if most_matches[1] >= 3:
+					break
 
 
-			if len(font_table) > 2:
-				most_matches = max(font_table.iteritems(), key=lambda (k,v): v)
+			print "Font table before incrementing count: ", font_table.items()
 
-				if most_matches[1] >=10:
-					print "This is most matches when it's exceeded 10:", most_matches
-					font = str(most_matches[0])
-					
-					break 
-
-			count +=1
-
+				
+		
+					#### Find a way to return the next few cases 
+			
 			print "This is the length of the font table at the end of the font loop: ", len(font_table) 
 			print "Count at the bottom of the ocr alphabet loop", count
-	
-	print most_matches
+			print "This is the font_table at the bottom of the loop: ", font_table.items()
+			count +=1
+
+	if len(font_table) <= 2:
+		# return "I'm sorry. It doesn't look like I can find a good match for you. Can you try another image?"
+		print "I'm sorry. It doesn't look like I can find a good match for you. Can you try another image?"
+	return font_table
+
+		
+	# else:
+	# 	print most_matches # do I need this because it should be 
 	
 
 
 # only works when images are identically sized 
-def difference_of_images(user_img, template_img): # passed in as PIL imgs
+def difference_of_images(user_img, template_img): # passed in as SimpleCV images
 
-	# loads pixel values into array 
-	pixel_user = np.asarray(user_img).flatten()
-	pixel_template = np.asarray(template_img).flatten()
+	user = cv.Image(user_img).binarize()
+	user_matrix = user.getNumpy().flatten()
+
+	template = cv.Image(template_img).binarize()
+	template_matrix = template.getNumpy().flatten()
+
+	# print "User matrix", user_matrix
+	# print 'Template matrix', template_matrix
 
 	# performs xor match 
-	difference = [i^j for i,j in zip(pixel_user, pixel_template)]	
-	difference2 = [i^j for i,j in zip(pixel_template, pixel_user)]
+	difference = [i^j for i,j in zip(user_matrix, template_matrix)]	
+	difference2 = [i^j for i,j in zip(template_matrix, user_matrix)]
+	# print "xor of difference1", difference
+	# print "xor of difference2", difference2
 
-	# number of times 1 (True) appears in the list --> pixels are exclusively different
-	diff= difference.count(1)
-	# print "This is the amount of difference: ", diff
-	diff2 = difference.count(1)
+	# # however many times 0 appears in the list
+	diff = difference.count(255)
+	# print "Count of difference in pixel", diff
 
+	diff2 = difference2.count(255)
+	# print "Count of difference in pixel", diff2
 
-	total_diff = float(diff) + float(diff2)
-	percent_of_diff = total_diff/float(len(pixel_user)+len(pixel_template))
+	total_diff = diff + diff2
+	xor = total_diff/float(len(user_matrix)+len(template_matrix))
 
-	print "Percentage of difference between the two images:", percent_of_diff*100
-	return round(percent_of_diff, 4)
-	# 1 means complete difference; 0 means complete same
-
-
-def identify_letter(ocr_data):
-
-	ocr_match_dict = {}
-
-	for key in ocr_data.iterkeys():
-		min_value = min(ocr_data[key]) # finds lowest % diff from list, 0 is a perfect match 
-		idx_pos = ocr_data[key].index(min_value)
-		print min_value, idx_pos
-		if idx_pos < 26:
-			letter = chr(idx_pos + 97) # if lower 
-		else:
-			letter = chr(idx_pos + 39) # if upper 
-		ocr_match_dict.setdefault(key, [min_value, idx_pos, letter])
-
-	return ocr_match_dict
-
-def get_letters_to_process(user_dir, ocr_match_dict):
-
-	segments = os.listdir(user_dir)
-	if '.DS_Store' in segments:
-		segments.remove('.DS_Store')
-
-	segments = sorted_nicely(segments)
-	letters_to_process = []
-
-	# output = ""
-	for imgfile in segments:
-
-		letter_tuple = []
-		img_url = os.path.abspath(os.path.join(user_dir, imgfile))	
-		letter = ocr_match_dict[img_url][2]
-
-		letter_tuple.append(img_url)
-		letter_tuple.append(letter)
-
-
-		letters_to_process.append(letter_tuple)
-
-		# letters_to_process.append(img_url)
-		# letters_to_process.append(letter)
-		# output += letter
-
-	# print "It looks like your image says: %s" % (output) 
-	return letters_to_process
-
-
-def match_font(process_letter_list):
-
-	letters = []
-	user_urls = []
-
-	for item in process_letter_list:
-		user_urls.append(item[0])
-		letters.append(item[1])
-
-	font_table = {}
-	n=0
-	while n < len(letters):
-		# print "This is n at the top: ", n 
-		user_img = Image.open(user_urls[n])
-		letter = letters[n] # gets you one letter
-		value = ord(letter)
-		fonts = model.session.query(model.Letter.file_url).filter(model.Letter.value == value).all()	
-
-		font_urls = []
-		for f in fonts:
-			font_urls.append(str(f[0])) # creates a list of template imgs that can be iterated through
-	
-		for url in font_urls:
-			
-			file_location = url 
-			font = Image.open(file_location)
-
-			if user_img.size[0] > font.size[0]:
-				# print "User_img.size is bigger than template size: ", user_img.size, template.size
-				# if user image is bigger, size it down to template size
-				user_img_resized = resize_to_smaller(user_img, font.size[0], font.size[1])
-				diff = difference_of_images(user_img_resized, font)
-
-
-			else:
-				font_resized = resize_to_smaller(font, user_img.size[0], user_img.size[1])
-				# print template_resized.size
-				diff = difference_of_images(user_img, font_resized)
-
-
-			# if diff <= 0.5:
-			font_name = model.session.query(model.Letter.font_name).filter(model.Letter.file_url==file_location).one()
-			# print "This is the fontname: ", font_name
-
-			if font_name not in font_table.keys():
-				font_table.setdefault(font_name, [diff])
-
-			else:
-				font_table[font_name].append(diff)
-
-		n+=1
-		# print 'This is n at the bottom: ', n 
-	return font_table
+	print "Percentage of difference between the two images:", xor*100
+	return xor
 
 
 def rank_fonts(font_table):
+
+	print "This is the font table"
+
 	for key, value in font_table.items():
-		print "Ranking fonts...\n"
-		print "Font, rate of difference:", key, value, '\n\n\n'  
+		print key, value, '\n'
 
-	least_difference = min(font_table.iteritems(), key=lambda (k,v): np.mean(v))
-	# print "This is the font with the least_difference with iteritems: ", least_difference
-	font = least_difference[0][0]
-
-	return "It looks like this is the font you're looking for: %s" % (font) 
-
+	most_matches = sorted(font_table.items(), key=lambda t: t[1])
+	print "\n\n\n\n\""
+	print "This is the ordered dictionary:"
+	for key, value in most_matches:
+		print key, value
 
 
-	
+
+	print "\n\n\n\n\n\n\n\n\n\n\n"
+	print "Making some decisions now:"
+
+	# if len(most_matches.items()) == 1:
+	# 	print "Match found! This is the closest match found: ", str(font_table.keys())
+
+	# if len(font_table.items()) > 1 and len(font_table.items()) < 4:
+	# 	# return top match 
+	# 	print "Match found! This is the closest match found: ", averageitems[0][0]
+
+	# if len(font_table.items()) >= 4:
+	# 	# if there are several matches	
+	# 	print "Match found: This is the closest match found: ", averageitems[0][0]
+		
+	# 	averageitems.pop(0)
+	# 	while averageitems [0][1] <= .05:
+	# 		print "These were also close matches: ", averageitems[0][0]
+
+
 
 
 def sorted_nicely(list):
@@ -279,29 +233,18 @@ def sorted_nicely(list):
     return sorted(list, key = alphanum_key)
 
 
+
+
 def main():
-
-
 	# process_user_image(directory='user_image') # commits user image to database
-
 	base_data = get_letter(user_dir='user_image', ocr_dir='ocr_alphabet')
 	segments = base_data[0]
 	ocr_alphabet = base_data[1]
 
+	font_table = find_match(user_dir='user_image', ocr_dir='ocr_alphabet/Arial', segments=segments, ocr_alphabet=ocr_alphabet)
+	results = rank_fonts(font_table)
 
-
-	find_match(user_dir='user_image', ocr_dir='ocr_alphabet/Arial', segments=segments, ocr_alphabet=ocr_alphabet)
-
-
-
-	
-	# result = rank_fonts(font_table)
-
-	# return result 
-
-	
-
-
+	print results 
 
 
 if __name__ == "__main__":
