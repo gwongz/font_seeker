a18d99cdd77e51da404c3dd8d6c7337d477c6a95
Grace Wong <gwongz@gmail.com>
2013-09-04 13:00:56 -0700

    Fixes so API only returns JSON

--- main.py
+++ main.py
@@ -24,34 +24,39 @@ def send_image():
 	try:
 		img_url = request.args.get('img') 
 		segments = get_segments.main(img_url)
-		result = json.dumps(segments)
+	
+		if segments == [] or len(segments) <= 1:
+			result = None
+		else:
+			result = json.dumps(segments)
 		
 	except IOError:
-		message = "Oh, snap. I can't find that image. Can you try a different image?"
-		return json.dumps(message) 
+		result = json.dumps(segments)
 
-	if len(segments) <=1 or segments == []:
-		return "I'm sorry, I'm not able to segment this image."
-	else:
-		return redirect(url_for('match_font'))
+	return redirect(url_for('match_font'))
+
+
+
 	
 	
 @app.route ('/match_font', methods = ['GET'])
 def match_font():
+
 	# returns list of strings
 	my_font = ranked_match.main() 
 
 	font_result = {}
+
+	# no matches
+	if my_font == []:
+		font_result["success"] = False
+	
 	if len(my_font) >= 1: 
 		font_result["success"] = True
 		for item in my_font:
 			font_result["font_name"] = item[0]
 			font_result["difference_value"] = item[1]
 			break
-
-	if len(my_font) == 0:
-		font_result["success"] = False
-		font_result["multiple"] = False 
 
 	result = json.dumps(font_result)
 	return result 
@@ -63,6 +68,7 @@ def home():
 @app.route ('/index', methods = ['GET'])
 def index():
 	return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
 	app.run(debug = True)
